fd1516951513cfcc13f00c1431ef2a1db6a34456
uu-ymar <arayou20040420@gmail.com>
2025-06-25 11:09:33 +0900

    SpringBoot課題

---
+++ BizConnect/src/test/java/com/biz/connect/BizConnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biz.connect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BizConnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
